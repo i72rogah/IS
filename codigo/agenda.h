cece81cee6022a368714c0ec8739d0d7558a6c1c
i72rogah <i72rogah@uco.es>
2018-12-09 20:56:04 +0100

    Cambios

--- codigo/agenda.h
+++ codigo/agenda.h
@@ -11,14 +11,16 @@ list<Alumno>alumnos_;
 
 public:
 void introducirAlumno();
-void modificarAlumno();
-void borrarAlumno();
+void modificarAlumno(std::string s);
+void borrarAlumno(std::string s);
 int buscarAlumno();
 void mostrarAlumnos();
 void mostarEquipo(int equipo);
 void mostrarAlumno();
 void ordenarAlf(std::string s);
 void ordenarAlf(int curso);
+inline list <Alumno> getAlumnos(){return alumnos_;};
+inline void setAlumnos(list <Alumno> aux){alumnos_=aux;};
 };
 
 
