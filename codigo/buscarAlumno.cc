d784f6861beb37d71153517975a1f120d2b59da1
i72roloa <antoniorodriguez664@gmail.com>
2018-12-22 17:39:03 +0100

    cambio en lista

--- codigo/buscarAlumno.cc
+++ codigo/buscarAlumno.cc
@@ -7,7 +7,9 @@ int buscarAlumno(std::string s){
   list <Alumno> aux;
   aux=getAlumnos();
   int cont=0;
-
+  if(aux.size()==0){
+    return -1:
+  }
   for(it=aux.begin();it!=aux.end();it++){
     if(it->getDNI()==s||it->getApellidos()==s){
       cont++;
